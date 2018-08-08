340f33132dfca76c74edb71e9a58d9728504e753
alex bunnell and james hanley <james@>
2018-08-08 16:28:05 -0700

    Add partial functionallity to a hangman game that uses the Dino Ipsum API to generate a random word for the user to guess. Adds the DinoHangman class.

--- src/dino.js
+++ src/dino.js
@@ -1,5 +1,60 @@
 //business logic
 
-// export function journal(goal) {
-//
-// }
+export class DinoHangman
+{
+  constructor(dinoName, blankSpots, guessedLetters = [], turnCounter = 0, turnLimit = 10)
+  {
+    this.dinoName = dinoName;
+    this.blankSpots = blankSpots;
+    this.guessedLetters = guessedLetters;
+    this.turnCounter = turnCounter;
+    this.turnLimit = turnLimit;
+  }
+
+  setName(newName)
+  {
+    this.dinoName = newName;
+  }
+  setBlanks(newBlanks)
+  {
+    this.blankSpots = newBlanks;
+  }
+
+  checkLetter(letter)
+  {
+    let truth = 0;
+    this.guessedLetters.push(letter);
+    for(let i = 0;i < this.dinoName.length;i++)
+    {
+      if(this.dinoName[i] == letter)
+      {
+        this.blankSpots[i] = letter;
+        truth++;
+      }
+    }
+    if(truth == 0)
+    {
+      this.turnCounter++;
+    }
+  }
+
+  checkGameOver()
+  {
+    let winCheck = 0;
+    for(let i = 0;i < this.dinoName.length; i++)
+    {
+      if(this.dinoName[i] == this.blankSpots[i])
+      {
+        winCheck++;
+      }
+    }
+    if(this.turnCounter == this.turnLimit || winCheck == this.dinoName.length)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 //logic for user interface
+import { DinoHangman } from './dino.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,23 +7,39 @@ import './styles.css';
 
 $(document).ready(function()
 {
+  //Global Variables along with any IDs we want hidden
+  let dinoGame = new DinoHangman();
   $("#dino-space").hide();
+  $("#dino-hangman").hide();
+  $("#header2").hide();
+  $("#dino-game-over").hide();
+  //Header Switcher
+  setInterval(function()
+  {
+    $("#header1").toggle();
+    $("#header2").toggle();
+  }, 3000);
+  //Button for accessing the dino ipsum feature.
   $("button#do-stuff").click(function()
   {
     $("#dino-space").show();
     $("#sweet-landing").hide();
   });
-
-  $("button#gimme-dino").click(function(event){
+  //Button for generating dino ipsum.
+  $("button#gimme-dino").click(function(event)
+  {
     event.preventDefault();
     let words = $('#words').val();
     $('#words').val("0");
     let paragraphs = $('#paragraphs').val();
     $('#paragraphs').val("0");
+
     let request = new XMLHttpRequest();
     let url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${paragraphs}&words=${words}`;
-    request.onreadystatechange = function(){
-      if(this.readyState === 4 && this.status === 200){
+    request.onreadystatechange = function()
+    {
+      if(this.readyState === 4 && this.status === 200)
+      {
         let response = JSON.parse(this.responseText);
         getElements(response);
       }
@@ -33,10 +50,54 @@ $(document).ready(function()
     let getElements = function(response)
     {
       let tempResults = response.join().split(",").join(", ");
-      console.log(tempResults);
-
       $('#results').html(`Your Dino-ipsum results are: ${tempResults}`);
     }
+  });
+  //Button for accessing the dino hangman feature
+  $("button#play-hangman-button").click(function(event)
+  {
+    event.preventDefault();
+    $("#dino-hangman").show();
+    $("#sweet-landing").hide();
 
+    let request = new XMLHttpRequest();
+    let url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1`;
+    request.onreadystatechange = function()
+    {
+      if(this.readyState === 4 && this.status === 200)
+      {
+        let response = JSON.parse(this.responseText.toLowerCase());
+        getElements(response);
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+
+    let getElements = function(response)
+    {
+      let displayArray = [];
+      let nameArray = response.join().split("");
+      let arrayLength = response.join().split("").length;
+      for(let i = 0; i < arrayLength; i++)
+      {
+        displayArray.push("_");
+      }
+      dinoGame.setName(nameArray);
+      dinoGame.setBlanks(displayArray);
+      $('#blanks').html(`Dinosaur Name: ${dinoGame.blankSpots}`);
+      $('#guesses').html(`Guessed Letters: ${dinoGame.guessedLetters}`);
+      $('#tries-left').html(`Tries Remaining: ${dinoGame.turnLimit - dinoGame.turnCounter}`);
+    }
+  });
+  //Button to submit letter guess
+  $("form#letter-guess-form").submit(function(event)
+  {
+    event.preventDefault();
+    let input = $("#letter-guess").val();
+    $("#letter-guess").val("");
+    dinoGame.checkLetter(input);
+    $('#blanks').html(`Dinosaur Name: ${dinoGame.blankSpots}`);
+    $('#guesses').html(`Guessed Letters: ${dinoGame.guessedLetters}`);
+    $('#tries-left').html(`Tries Remaining: ${dinoGame.turnLimit - dinoGame.turnCounter}`);
   });
 });
