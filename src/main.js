b0903d571793588fb5ac817c57b3c50be54d9457
alex bunnell and james hanley <alex+james@>
2018-08-08 10:45:47 -0700

    dino works

--- src/main.js
+++ src/main.js
@@ -6,9 +6,37 @@ import './styles.css';
 
 $(document).ready(function()
 {
+  $("#dino-space").hide();
   $("button#do-stuff").click(function()
   {
     $("#dino-space").show();
+    $("#sweet-landing").hide();
   });
 
+  $("button#gimme-dino").click(function(event){
+    event.preventDefault();
+    let words = $('#words').val();
+    $('#words').val("0");
+    let paragraphs = $('#paragraphs').val();
+    $('#paragraphs').val("0");
+    let request = new XMLHttpRequest();
+    let url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${paragraphs}&words=${words}`;
+    request.onreadystatechange = function(){
+      if(this.readyState === 4 && this.status === 200){
+        let response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+
+    let getElements = function(response)
+    {
+      let tempResults = response.join().split(",").join(", ");
+      console.log(tempResults);
+
+      $('#results').html(`Your Dino-ipsum results are: ${tempResults}`);
+    }
+
+  });
 });
